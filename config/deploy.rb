349960be69931213676ae7f7054bc7fe517aadae
Stuart Watson <stuart.watson@unep-wcmc.org>
2018-08-13 13:17:10 +0100

    Changed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/strtwtsn/hello_world.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/ubuntu"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
